eaceac7ff55e2e081d8a76499cf064f47a7ed2bd
pivobrstore-create <pivobrstore@gmail.com>
2025-11-28 07:28:04 -0300

    Update telegramSync.js

--- telegramSync.js
+++ telegramSync.js
@@ -1,26 +1,37 @@
 const axios = require('axios');
+const fs = require('fs');
 
 
 const TELEGRAM_TOKEN = process.env.BOT_TOKEN;
-const CHANNEL_USERNAME = process.env.CHANNEL_USERNAME;
+const CHANNEL_ID = process.env.CHANNEL_ID;
 
 
-async function buscarUltimaOferta() {
+async function buscarUltimaOfertaCompleta() {
 const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/getUpdates`;
 const { data } = await axios.get(url);
 
 
-const mensagens = data.result.filter(m => m.channel_post);
-const ultima = mensagens[mensagens.length - 1];
+const posts = data.result.filter(p => p.channel_post);
+const ultima = posts[posts.length - 1];
 
 
 if (!ultima) return null;
 
 
+let imagem = null;
+if (ultima.channel_post.photo) {
+const fileId = ultima.channel_post.photo.pop().file_id;
+const filePath = await axios.get(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/getFile?file_id=${fileId}`);
+const fileUrl = `https://api.telegram.org/file/bot${TELEGRAM_TOKEN}/${filePath.data.result.file_path}`;
+imagem = fileUrl;
+}
+
+
 return {
 texto: ultima.channel_post.caption || ultima.channel_post.text || "",
+imagem
 };
 }
 
 
-module.exports = { buscarUltimaOferta };
+module.exports = { buscarUltimaOfertaCompleta };
